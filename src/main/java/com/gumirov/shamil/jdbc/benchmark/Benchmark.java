1d98bdb15475b9c04d65ce4824321effc453434f
Shamil Gumirov <shamil@gumirov.com>
2017-02-03 22:37:57 +0700

    initial add

---
+++ src/main/java/com/gumirov/shamil/jdbc/benchmark/Benchmark.java
@@ -0,0 +1,83 @@
+package com.gumirov.shamil.jdbc.benchmark;
+
+import org.postgresql.pljava.annotation.*;
+
+import java.sql.*;
+import java.util.Random;
+
+/**
+ * Created by phoenix on 2/3/17.
+ */
+public class Benchmark {
+  static final String insert = "INSERT INTO %s VALUES(?,?)";
+  static final String select = "SELECT id, val FROM %s WHERE id = ?";
+  static final String delete = "DELETE FROM %s";
+  
+  //@Mock
+  public static String url = "jdbc:default:connection";
+  
+  @Function
+  public static String fillTable(String table, int start, int end) throws SQLException {
+    long t = System.currentTimeMillis();
+
+    Connection con = DriverManager.getConnection(url);
+
+    PreparedStatement pst = con.prepareStatement(String.format(insert, table));
+
+    for (int i = start; i < end; ++i){
+      pst.setInt(1, i);
+      pst.setInt(2, i);
+      pst.executeUpdate();
+    }
+    
+    pst.close();
+    con.close();
+
+    t = System.currentTimeMillis() - t;
+
+    return "Time used: "+t+" ms";
+  }
+
+  @Function
+  public static String randomReads(String table, int number, int max) throws SQLException {
+    long t = System.currentTimeMillis();
+
+    Connection con = DriverManager.getConnection(url);
+
+    PreparedStatement pst = con.prepareStatement(String.format(select, table));
+    Random r = new Random(System.currentTimeMillis());
+    int avg = 0;
+
+    for (int i = 0; i < number; ++i){
+      int id = r.nextInt(max-1)+1;
+      pst.setInt(1, id);
+      ResultSet rs = pst.executeQuery();
+      if (!rs.first()) throw new RuntimeException("No data at id="+id);
+      avg = (avg + rs.getInt(2))>>1;
+    }
+
+    pst.close();
+    con.close();
+    
+    t = System.currentTimeMillis() - t;
+
+    return "Avg = "+avg+". Time used: "+t+" ms";
+  }
+
+  @Function
+  public static String cleanTable(String table) throws SQLException {
+    long t = System.currentTimeMillis();
+
+    Connection con = DriverManager.getConnection(url);
+
+    Statement s = con.createStatement();
+    s.executeUpdate(String.format(delete, table));
+    
+    s.close();
+    con.close();
+
+    t = System.currentTimeMillis() - t;
+
+    return "Time used: "+t+" ms";
+  }
+}
---
+++ src/main/java/com/gumirov/shamil/jdbc/benchmark/LocalTestRun.java
@@ -0,0 +1,33 @@
+package com.gumirov.shamil.jdbc.benchmark;
+
+import java.sql.SQLException;
+
+/**
+ * Created by phoenix on 2/3/17.
+ */
+public class LocalTestRun{
+  public static void main(String[] args) throws SQLException {
+    mysqlTest();
+//    postgresTest();
+  }
+
+  private static void mysqlTest() throws SQLException {
+    Benchmark.url = "jdbc:mysql://127.0.0.1:3306/test?user=root&password=gfhjkm";
+    final String table = "test_table";
+    final int MAX = 1000;
+    System.out.println("Starting test..");
+    System.out.println(Benchmark.fillTable(table, 1, MAX));
+    System.out.println(Benchmark.randomReads(table, MAX, MAX));
+    Benchmark.cleanTable(table);
+  }
+
+  private static void postgresTest() throws SQLException {
+    Benchmark.url = "jdbc:postgresql://192.168.3.100/test?user=root&password=gfhjkm";
+    final String table = "test_table";
+    final int MAX = 1000;
+    System.out.println("Starting test..");
+    System.out.println(Benchmark.fillTable(table, 1, MAX));
+    System.out.println(Benchmark.randomReads(table, MAX, MAX));
+    Benchmark.cleanTable(table);
+  }
+}
