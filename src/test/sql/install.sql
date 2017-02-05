413155f6fd00f1e570ad73dbbd24204f74f96d86
Shamil Gumirov <shamil@gumirov.com>
2017-02-05 15:22:32 +0700

    added index

--- src/test/sql/install.sql
+++ src/test/sql/install.sql
@@ -1,4 +1,5 @@
-SELECT sqlj.install_jar('file:/tmp/JdbcBenchmark-1.0-SNAPSHOT.jar', 'benchmark', true);
+SELECT sqlj.replace_jar('file:/tmp/JdbcBenchmark-1.0-SNAPSHOT.jar', 'benchmark', true);
 SELECT sqlj.set_classpath('public', 'benchmark');
 DROP TABLE IF EXISTS test_table;
 CREATE TABLE test_table (id INTEGER PRIMARY KEY, val integer);
+CREATE INDEX test_table_idx test_table(id);
