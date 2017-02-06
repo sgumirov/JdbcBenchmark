bc7f2c9ba18fdf655e72d4a48ecebf0040d5db05
Shamil Gumirov <shamil@gumirov.com>
2017-02-06 15:24:01 +0700

    merged

--- src/test/sql/install.sql
+++ src/test/sql/install.sql
@@ -1,4 +1,4 @@
-SELECT sqlj.install_jar('file:/tmp/JdbcBenchmark-1.0-SNAPSHOT.jar', 'benchmark', true);
+SELECT sqlj.replace_jar('file:/tmp/JdbcBenchmark-1.0-SNAPSHOT.jar', 'benchmark', true);
 SELECT sqlj.set_classpath('public', 'benchmark');
 DROP TABLE IF EXISTS test_table;
 CREATE TABLE test_table (id INTEGER PRIMARY KEY, val integer);
