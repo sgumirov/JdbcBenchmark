54ff1f772c35b28745834d0f855869651f2ee131
Shamil Gumirov <shamil@gumirov.com>
2017-02-06 19:36:15 +0700

    mnor

--- src/test/sql/exec.sql
+++ src/test/sql/exec.sql
@@ -1,4 +1,6 @@
 SELECT public.fillTable('test_table', 1, 100);
-SELECT public.randomReads('test_table', 100000, 100);
-SELECT public.randomReadsNoPrep('test_table', 100000, 100);
+SELECT public.randomReads('test_table', 1000, 100);
+SELECT public.randomReadsNoPrep('test_table', 1000, 100);
+SELECT public.randomReads('test_table', 1000, 100);
+SELECT public.randomReadsNoPrep('test_table', 1000, 100);
 SELECT public.cleanTable('test_table');
