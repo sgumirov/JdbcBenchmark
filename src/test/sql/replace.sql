e815f28e8a9afc2faffdf7c551a808cde00c6beb
Shamil Gumirov <shamil@gumirov.com>
2017-02-06 18:25:55 +0700

    added replace

--- src/test/sql/replace.sql
+++ src/test/sql/replace.sql
@@ -2,4 +2,4 @@ SELECT sqlj.replace_jar('file:/tmp/JdbcBenchmark-1.0-SNAPSHOT.jar', 'benchmark',
 SELECT sqlj.set_classpath('public', 'benchmark');
 DROP TABLE IF EXISTS test_table;
 CREATE TABLE test_table (id INTEGER PRIMARY KEY, val integer);
-CREATE INDEX test_table_idx test_table(id);
+CREATE INDEX test_table_idx ON test_table(id);
